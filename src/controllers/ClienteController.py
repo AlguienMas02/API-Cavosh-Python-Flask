f09d586c8014ac70cd4f5e22bd0871873e3bfe97
AlguienMas020 <mpaulinodurand2015@gmail.com>
2025-12-02 22:14:10 -0500

    controller

--- src/controllers/ClienteController.py
+++ src/controllers/ClienteController.py
@@ -13,4 +13,42 @@ class ClienteController:
         
         if usuario:
             return {'mensaje': 'Login exitoso', 'usuario': usuario.to_dict()}, 200
+        else:
+            return {'error': 'Datos incorrectos'}, 401
 
+    @staticmethod
+    def guardar():
+        data = request.json
+        _id = data.get('id', 0)
+        nombre = data.get('nombres')
+        correo = data.get('correo')
+        passwordd = data.get('passwordd')
+
+        response = ClienteService.registrar_o_actualizar(_id, nombre, correo, passwordd)
+        
+        if 'error' in response:
+            return jsonify(response), 404
+            
+        # Agregamos mensaje de éxito para el frontend
+        response['mensaje'] = 'Código generado'
+        return response, 200
+    
+    @staticmethod
+    def validarCodigo():
+        data = request.json
+        _id = data.get('id')
+        _codigo = data.get('codigo')
+            
+        response = ClienteService.validar_codigo(_id, _codigo)
+        
+        if 'error' in response:
+            return response, 404
+            
+        if not response.get('valido'):
+            return response, 400
+            
+        return response, 200
+                
+        
+        
+        
